91d86d73b3238cab27e76300d5b6f4c50bb0276b
ManojKumar184 <kanaparthimanojkumar184@gmail.com>
2025-10-21 13:19:41 +0530

    navbar change for mobile layout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,14 @@
-import { useState } from "react";
-import { useAuth } from "../context/AuthContext.jsx";
 import { Users, LogOut } from "lucide-react";
+import { useAuth } from "../context/AuthContext.jsx";
 
-export default function Navbar() {
+export default function Navbar({ sidebarOpen, setSidebarOpen }) {
   const { user, logout } = useAuth();
-  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <header className="flex justify-between items-center bg-indigo-600 text-white px-6 py-3 shadow-md z-50 w-full">
       {/* Left: Sidebar toggle */}
-      <button 
-        onClick={() => setSidebarOpen(prev => !prev)}
+      <button
+        onClick={() => setSidebarOpen(!sidebarOpen)}
         className="md:hidden"
       >
         <Users className="w-6 h-6" />
