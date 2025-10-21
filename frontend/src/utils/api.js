4790fc3b7047a89cc29dc5a18b7b9cd4502c3b3a
Manoj Kumar <152824312+ManojKumar184@users.noreply.github.com>
2025-10-21 12:21:46 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://chatterbox-9tlu.onrender.com";
 
 const getConfig = (token) => ({
   headers: {
